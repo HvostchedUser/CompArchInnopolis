86d41ccf512989a0ba37545e004c34fe7cb0bee4
HvostchedUser <42430176+HvostchedUser@users.noreply.github.com>
2020-09-25 01:10:10 +0300

    123

---
+++ Week4/Verilog2.v
@@ -0,0 +1,12 @@
+module Verilog1(
+	output [7:0] HEX0,
+	output [7:0] HEX1,
+	output [7:0] HEX2,
+	output [7:0] HEX3,
+	output [7:0] HEX4,
+	output [7:0] HEX5,
+	input [1:0] KEY
+);
+//date of birth
+//04.01.2002
+
